2695f78335224fab8900bce56fc0c8758ea6712d
deweezy12 <55617587+deweezy12@users.noreply.github.com>
2022-09-04 22:37:29 +0200

    Add files via upload

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -7,26 +7,30 @@ filename = 'data/death_valley_2018_simple.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
+	print(header_row)
 
 	#get high and low temepratures from this file and dates
-	dates, highs, lows = [], [], []
+	dates, highs, lows, rains = [], [], [], []
 	for row in reader:
 		current_date = datetime.strptime(row[2], '%Y-%m-%d')
 		try:
+			rain = float(row[3])
 			high = int(row[4])
-			low = int(row[5])
+			low = int(row[5])			
 		except ValueError:
 			print(f"Missing data for {current_date}")
 		else:
 			dates.append(current_date)
+			rains.append(rain)
 			highs.append(high)
 			lows.append(low)
-
+			
 #plot the high and lowtemperatures
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red', alpha=0.5)
 ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.plot(dates,rains, c='green', alpha=0.5)
 ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 #format plot
