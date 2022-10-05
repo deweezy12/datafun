1a0d6b338509c6bc3d08b28729a330d9ec1411c1
deweezy12 <55617587+deweezy12@users.noreply.github.com>
2022-10-06 00:02:02 +0200

    Add files via upload

---
+++ python_repos.py
@@ -0,0 +1,13 @@
+import requests
+
+#make an API call and store the response
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers ={'Accept': 'applitcation/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+#store API response in a variable
+response_dict = r.json()
+
+#process results
+print(response_dict.keys())
